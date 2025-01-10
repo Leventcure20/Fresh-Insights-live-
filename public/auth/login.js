8fad2039cc19169fe4132cffbaf03759366adf1c
Levent Cure <leventcure@Levents-MacBook-Air.local>
2025-01-09 20:10:51 -0600

    Switched from bcrypt to bcryptjs

--- public/auth/login.js
+++ public/auth/login.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "../db/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth20";
